f27a23ee3c2d4937a2e75f7ddce70459114f3c47
anventia <brendanchen006@gmail.com>
2021-10-27 23:11:34 -0700

    Update files

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, guildId2, guildId3, token } = require('./config.json');
+// 1: ANQRDR, 2: Utility Server, 3: CF
 
 const commands = []
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -15,5 +16,17 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => console.log('Successfully registered application commands 1.'))
 	.catch(console.error);
+    
+const rest2 = new REST({ version: '9' }).setToken(token);
+
+rest2.put(Routes.applicationGuildCommands(clientId, guildId2), { body: commands })
+    .then(() => console.log('Successfully registered application commands 2.'))
+    .catch(console.error);
+        
+const rest3 = new REST({ version: '9' }).setToken(token);
+
+rest3.put(Routes.applicationGuildCommands(clientId, guildId3), { body: commands })
+    .then(() => console.log('Successfully registered application commands 3.'))
+    .catch(console.error);
--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		client.user.setActivity("for Slash Commands", {  // Set status activity
+			type: "WATCHING"
+		  });
 		console.log(`Bot Ready! Logged in as ${client.user.tag}`);
 	},
 };
