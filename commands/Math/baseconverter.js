bffa87f8a280ddaaf2120ce72c6dc03b1163d702
anventia <brendanchen006@gmail.com>
2022-09-10 21:55:20 -0700

    Update baseconverter.js

--- commands/Math/baseconverter.js
+++ commands/Math/baseconverter.js
@@ -13,47 +13,61 @@ module.exports = {
         )
         .addStringOption(option => option
             .setName('start')
-            .setDescription('Starting base')
+            .setDescription('Starting base (2-62)')
             .setRequired(true)
         )
         .addStringOption(option => option
             .setName('target')
-            .setDescription('Target base')
+            .setDescription('Target base(2-62)')
             .setRequired(true)
         ),
     async execute(client, interaction) {
         // Setup //
-        const value  = interaction.options.getString("value").toUpperCase();  // Gather inputs
-        const start  = interaction.options.getString("start");
-        const target = interaction.options.getString("target");
-        const numbers = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+        var value  = interaction.options.getString("value").replace(" ", "");  // Gather inputs
+        const start  = BigInt(interaction.options.getString("start"));
+        const target = BigInt(interaction.options.getString("target"));
+        const numbers = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");  // (62) All the characters used as numbers.
+        if(start < 2 || target < 2 || start > numbers.length || target > numbers.length) {  // Limit bases from 2 - [numbers]
+            await interaction.reply({ content: "Error: Base value is invalid!", ephemeral: true }); return ;
+        }
+        if(start <= 36) { value = value.toUpperCase(); } // Automatically capitalize letters if starting base is 36 or lower
 
 
         // Convert value to Base 10  //
-        var valueB10 = 0;  // value converted into Base 10
-        const valueREV = value.toString().split("").reverse();  // Makes list of the characters in value, but reversed
-        for(let i=0; i<valueREV.length; i++) {
-            let char = valueREV[i].toString();  // Current character
-            let charVAL = numbers.indexOf(char);  // Value of char, e.g. 1 -> 1, F -> 15
-            if(parseInt(charVAL) >= start) {  // make sure input matches starting base, e.g. "1234" is not valid for base 2
-                await interaction.reply({ content: "Error: Invalid inputs!", ephemeral: true }); return ;
+        var valueB10 = BigInt(0);  // value converted into Base 10
+        const valueRev = value.toString().split("").reverse();  // Makes list of the characters in value, but reversed
+        for(let i=BigInt(0); i<valueRev.length; i++) {
+            let char = valueRev[i].toString();  // Current character
+            let charVal = BigInt(numbers.indexOf(char));  // Value of char, e.g. 1 -> 1, F -> 15
+            if(charVal >= start+1n) {  // make sure input matches starting base, e.g. "1234" is not valid for base 2
+                await interaction.reply({ content: "Error: Value and base incompatible!", ephemeral: true }); return ;
             }
-            valueB10 += (Math.pow(start,i))*charVAL;  // Increment result
+            const startB = BigInt(start);
+            valueB10 += (startB**i)*charVal;  // Increment result
         }
         
 
         // Convert from Base 10 to Target Base //
-        var valueTAR = "";  // value converted into the Target Base
+        var valueTar = "";  // value converted into the Target Base
         //valueB10REV = valueB10.toString().split("").reverse();  // Makes list of the characters in valueB10, but reversed
-        var valueB10T = parseInt(valueB10);  // Temp value for division
+        var valueB10T = BigInt(valueB10);  // Temp value for division
         for(let i=0; valueB10T > 0; i++) {
-            let mod = parseInt(valueB10T) % parseInt(target);  // Gets the remainder from operation
-            if(true) { valueTAR = numbers[mod] + valueTAR; }  // Increment result
-            valueB10T = Math.floor(parseInt(valueB10T) / parseInt(target));  // Sets valueB10 to remaining number after division
+            let mod = valueB10T % target;  // Gets the remainder from operation
+            valueTar = numbers[Number(mod)] + valueTar;  // Increment result
+            valueB10T = valueB10T / target;  // Sets valueB10 to remaining number after division
         }
+        if(valueTar.length > 1000) {valueTar = valueTar.substring(0,1000)+" ..."; }
+
 
         // Send message // (update this with embed later!)
-        await interaction.reply(`Base ${start}:\n\`\`\`${value}\`\`\`\nBase 10:\n\`\`\`${valueB10}\`\`\`\nBase ${target}:\n\`\`\`${valueTAR}\`\`\``)
-        
+        //await interaction.reply(`Base ${start}:\n\`\`\`${value}\`\`\`\nBase 10:\n\`\`\`${valueB10}\`\`\`\nBase ${target}:\n\`\`\`${valueTar}\`\`\``)
+        const inputEmbed = new MessageEmbed()
+		    .setColor(global.embedBlue)
+		    .addField(`Input (Base ${start})`, `\`\`\`${value}\`\`\``);
+        await interaction.reply({embeds: [inputEmbed]});
+        const outputEmbed = new MessageEmbed()
+		    .setColor(global.embedBlue)
+		    .addField(`Output (Base ${target})`, `\`\`\`${valueTar}\`\`\``);
+        await interaction.channel.send({embeds: [outputEmbed]});
     }
 }
