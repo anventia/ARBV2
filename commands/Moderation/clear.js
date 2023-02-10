df75111d2beca66deb83fb1eb903b02e98030fe4
anventia <brendanchen006@gmail.com>
2023-02-10 15:29:08 -0800

    Added /clear

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -8,17 +8,17 @@ module.exports = {
 		.setDescription("Clears a certian amount of messages from a channel. (Mod Only)")
         .addIntegerOption(option => option
             .setName("amount")
-            .setDescription("Amount of messages to clear. [0 < n < 100]")
+            .setDescription("Amount of messages to clear. [0 < n <= 100]")
             .setRequired(true)
         ),
 	async execute(client, interaction) {
         let amount = interaction.options.getInteger("amount");
         if(amount > 100 || amount < 1) {
-            await f.sendMessage(`Amount must be in range 0-100!`, embedRed, interaction, "reply", true);
+            await f.sendMessage(`Amount must be in range 1-100!`, embedRed, interaction, "reply", true);
             return;
         }
         if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
-            await f.sendMessage(`You are missing the Manage Messages permissions!`, embedRed, interaction, "reply", false);
+            await f.sendMessage(`You are missing the Manage Messages permission!`, embedRed, interaction, "reply", true);
             return;
         }
 
